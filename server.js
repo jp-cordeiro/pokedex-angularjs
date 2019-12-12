4b1a1bd4a3893d0f965fd758e8bebc30ebec93e9
jp-cordeiro <jpc.araujo@hotmail.com>
2019-12-12 00:24:26 -0300

    heroku config

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.use(express.static(__dirname));
-app.get(/.*/, function (req,res) {
-    res.sendfile(__dirname + "/index.html")
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(port);
 
-console.log('server started');
+app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.listen(process.env.PORT || 3000);
